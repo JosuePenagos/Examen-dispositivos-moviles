a8bbc961bb19ccfc80cbc1754e7663d59540df33
JosuePenagos <josue.penagos261@pascualbravo.edu.co>
2026-05-11 21:23:02 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Flutter3/screens/contact_form.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //Título actualizado para el enunciado 
+      title: 'EduPlatform - Registro',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true, 
+      ),
+      home: const ContactForm(),
+    );
+  }
+}
 
